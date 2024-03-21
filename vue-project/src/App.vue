5c7938ab1d1c6c80fa3367246bb2cbf955dff68f
Justkim666 <tranhoangkim010702@gmail.com>
2024-03-21 20:58:19 +0700

    khoi tao du an

---
+++ vue-project/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page">
+    <h1>Hello, Vue.js!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
