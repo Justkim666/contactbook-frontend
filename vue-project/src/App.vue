37077095850dd918caf62980b36f0977f86ddebe
Justkim666 <tranhoangkim010702@gmail.com>
2024-03-22 08:33:37 +0700

    Cai dat trang loi 404

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,17 +1,38 @@
+<script>
+  import AppHeader from "@/components/AppHeader.vue";
+  import InputSearch from "./components/InputSearch.vue";
+  import ContactList from "./components/ContactList.vue";
+  import ContactCard from "./components/ContactCard.vue";
+  
+  export default {
+    components: {
+      AppHeader,
+    },
+  };
+</script>
+
 <template>
-  <div class="page">
-    <h1>Hello, Vue.js!</h1>
+  <div id="app">
+    <AppHeader />
+    <div class="container mt-3">
+      <div class="row">
+        <div class="col-md-4">
+          <InputSearch />
+          <ContactList :contacts="contacts" :activeIndex="activeIndex" @update:activeIndex="updateActiveIndex" />
+        </div>
+        <div class="col-md-8">
+          <ContactCard v-if="selectedContact" :contact="selectedContact" />
+        </div>
+      </div>
+      <router-view /> <!-- Đây là nơi router sẽ render component -->
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-}
-</script>
 
 <style>
-.page {
-  max-width: 400px;
-  margin: auto;
-}
+  .page {
+    max-width: 800px;
+    margin: auto;
+  }
 </style>
