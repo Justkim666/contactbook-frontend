4e9f96d80fe9eab6637d3580faf5bd983a31676d
Justkim666 <tranhoangkim010702@gmail.com>
2024-03-22 11:53:08 +0700

    Tao trang them lien he

---
+++ vue-project/src/views/AddContact.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2>Thêm liên hệ</h2>
+    <ContactFormAdd @submit:contact="addContact" />
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+import ContactFormAdd from "@/components/ContactFormAdd.vue"; // Thay đổi từ ContactForm sang ContactFormAdd
+import ContactService from "@/services/contact.service";
+
+export default {
+  name: 'addContact',
+  components: {
+    ContactFormAdd, // Sửa thành ContactFormAdd
+  },
+  data() {
+    return {
+      message: "",
+    };
+  },
+  methods: {
+    async addContact(data) {
+      try {
+        await ContactService.create(data);
+        this.message = "Liên hệ được thêm thành công.";
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
